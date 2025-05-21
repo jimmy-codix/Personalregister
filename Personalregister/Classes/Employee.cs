3bce5f4691951fda0a21e9817f27e6a14c0aca14
Jim <203992881+jayhoff@users.noreply.github.com>
2025-05-21 14:44:20 +0200

    Added a Employee class for each employee.

---
+++ Personalregister/Classes/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personalregister.Classes
+{
+    internal class Employee
+    {
+        private string surname;
+        private string lastname;
+        //TODO maybe change to floating point.
+        private int sallary;
+        public Employee(string surname, string lastname, int sallary) 
+        {
+            this.surname = surname;
+            this.lastname = lastname;
+            this.sallary = sallary;
+        }
+
+        public string GetSurname()
+        {
+            return surname;
+        }
+
+        public string GetLastname()
+        {
+            return lastname;
+        }
+
+        public int GetSallary()
+        {
+            return sallary;
+        }
+
+    }
+}
